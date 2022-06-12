2a003151463e5a887fae7554597d5acce0cb5b3c
kenbow98 <kenbow.998@gmail.com>
2022-06-12 04:39:01 +0000

    AcriveStorage 導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.active_job.queue_adapter = :inline
+
   config.hosts << "3849b7acd16649e695c65095f26faf3c.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
