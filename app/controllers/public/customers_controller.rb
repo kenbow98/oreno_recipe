a18475cce91c030d91b6419098588e3924dd4551
kenbow98 <kenbow.998@gmail.com>
2022-06-15 10:52:23 +0000

    コントローラー、ルーティング見直し

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,10 @@
 class Public::CustomersController < ApplicationController
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'relationships/followers'
+    get 'relationships/followings'
+  end
+
     #管理者用
   #URL /admin/sign_in ...
   devise_for :admins, contollers: {
@@ -20,7 +25,18 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root 'homes#top'
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+    # customers/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている
     get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customers/information' => 'customers#update', as: 'update_information'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    put 'customers/information' => 'customers#update'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+    resources :recipes, only: [:new, :index, :show]
+    
+    get 'followers' => 'relationships/followers', as: 'followers'
+    get 'followings' => 'relationships/followings', as: 'followings'
+
   end
 
 end
