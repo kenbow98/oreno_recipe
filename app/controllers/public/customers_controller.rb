5839c1cce44cd79aa41fbd25f9f3ce34fbc03467
kenbow98 <kenbow.998@gmail.com>
2022-06-25 12:38:41 +0000

    中断

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,39 @@
 class Public::CustomersController < ApplicationController
-  def edit
-  end
-
   def index
+    customers = Customer.where.not(name: "ゲストユーザー").deleted
+    @customer = current_customer
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    
   end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+      if @customer.update(customer_params)
+        flash[:notice] = "会員情報の編集に成功しました"
+        redirect_to public_customer_path(@user)
+      else
+        flash[:notice] = "会員情報の編集に失敗しました"
+        render :edit
+      end
+  end
+
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:nickname, :profile_image)
+  end
+  
+  def ensure_guest_user
+    @customer = Customer.find(params[:id])
+    if @customer.name == "guestuser"
+      redirect_to customer_path(current_customer) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
 end
